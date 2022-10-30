a1c4326b40330f1f741b6462b3cb4529d1ac126d
pooya Games <426713+pooyaEst@users.noreply.github.com>
2022-10-30 18:06:28 +0330

    define custom props using typescript generics

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,95 +1,92 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
-import React, { useContext, useState } from 'react';
-import { DarkModeContainer, Header, MainContainer, NavBar, Options, OptionsHolder, Page, Toggler } from './styles';
-import {BsFillMoonFill,BsMoon} from 'react-icons/bs'
-import {AiOutlineMenu} from 'react-icons/ai'
-import { Dark } from '../helper/Dark';
-import Link from 'next/link';
-import { ActivePage } from '../helper/ActivePage';
-import Head from 'next/head';
+import React, { useContext, useState } from "react";
+import {
+	DarkModeContainer,
+	Header,
+	MainContainer,
+	NavBar,
+	Options,
+	OptionsHolder,
+	Page,
+	Toggler,
+} from "./styles";
+import { BsFillMoonFill, BsMoon } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Dark } from "../helper/Dark";
+import Link from "next/link";
+import { ActivePage } from "../helper/ActivePage";
+import Head from "next/head";
 
-const Layout = (props:any) => {
+const Layout = (props: any) => {
+	const { darkMode, setDarkMode }: any = useContext(Dark);
+	const { active, setActive }: any = useContext(ActivePage);
 
-    const {darkMode, setDarkMode}:any = useContext(Dark);
-    const {active, setActive}:any = useContext(ActivePage);
-    
-    const [showList, setShowList]:any= useState(false); 
+	const [showList, setShowList]: any = useState(false);
 
-    function toggleHandler(){
-        setShowList((prev:boolean)=>!prev)
-    }
+	function toggleHandler() {
+		setShowList((prev: boolean) => !prev);
+	}
 
-    return (
-        
-        <Page
-         dark={darkMode}
-         onClick={()=>{
-            if(showList){setShowList(false)}
-         }}>
-            <Head>
-                <title>LandSurfer</title>
-                <link rel='icon' href='/globe.png'/>
-            </Head>
-            <Header  dark={darkMode}>
-                <Link href='/'>
-                    <h3> LandSurfer</h3>
-                </Link>
+	return (
+		<Page
+			dark={darkMode}
+			onClick={() => {
+				if (showList) {
+					setShowList(false);
+				}
+			}}
+		>
+			<Head>
+				<title>LandSurfer</title>
+				<link rel="icon" href="/globe.png" />
+			</Head>
+			<Header dark={darkMode}>
+				<Link href="/">
+					<h3> LandSurfer</h3>
+				</Link>
 
-            
+				<NavBar onMouseEnter={toggleHandler} onMouseLeave={toggleHandler}>
+					<Toggler dark={darkMode} show={showList} onClick={toggleHandler}>
+						<AiOutlineMenu />
+					</Toggler>
 
-                <NavBar
-                  onMouseEnter={toggleHandler}
-                  onMouseLeave={toggleHandler} >
-                    <Toggler
-                     dark={darkMode} 
-                     show={showList}
-                     onClick={toggleHandler}>
-                        <AiOutlineMenu />
-                    </Toggler>
-                    
-                    <OptionsHolder
-                     show={showList}
-                     dark={darkMode}>
-                    <Link href='/allCountries'>
-                        <Options 
-                         dark={darkMode}
-                         activePage={active=='All'}>
-                            All Countries
-                        </Options>
-                    </Link>
+					<OptionsHolder show={showList} dark={darkMode}>
+						<Link href="/allCountries">
+							<Options dark={darkMode} activePage={active == "All"}>
+								All Countries
+							</Options>
+						</Link>
 
+						<Link href="/continents">
+							<Options dark={darkMode} activePage={active == "Continents"}>
+								Continents
+							</Options>
+						</Link>
 
-                    <Link href='/continents'>
-                        <Options 
-                         dark={darkMode}
-                         activePage={active=='Continents'} >
-                            Continents
-                        </Options>
-                    </Link>
+						<Link href="/search">
+							<Options dark={darkMode} activePage={active == "Search"}>
+								Search
+							</Options>
+						</Link>
+					</OptionsHolder>
+				</NavBar>
 
-                    <Link href="/search">
-                        <Options  
-                         dark={darkMode}
-                         activePage={active=='Search'} >
-                            Search
-                        </Options>
-                    </Link>
-                    </OptionsHolder>
-                </NavBar>
-
-                <DarkModeContainer dark={darkMode} onClick={()=>setDarkMode((perv:boolean)=>!perv)}>
-                   {darkMode ? <BsFillMoonFill /> :<BsMoon />}
-                    <p>Dark Mode</p>
-                </DarkModeContainer>
-            </Header>
-            <MainContainer>
-            <main>{props.children}</main>
-            </MainContainer>
-        </Page>
-    );
+				<DarkModeContainer
+					dark={darkMode}
+					onClick={() => setDarkMode((perv: boolean) => !perv)}
+				>
+					{darkMode ? <BsFillMoonFill /> : <BsMoon />}
+					<p>Dark Mode</p>
+				</DarkModeContainer>
+			</Header>
+			<MainContainer>
+				<main>{props.children}</main>
+			</MainContainer>
+		</Page>
+	);
 };
 
 export default Layout;
--- components/layout/styles.tsx
+++ components/layout/styles.tsx
@@ -1,132 +1,132 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
-export const Page = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: ${props=>(props.dark? 'rgba(43, 43, 44,1)': '#fafafa')};
-    overflow: hidden;
-`
+type CustomProps = {
+	dark: boolean;
+	show?: boolean;
+};
 
-export const Header = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background-color: ${props=>(props.dark? 'rgb(43, 43, 44)': '#fff')};
-    color: ${props =>(props.dark?'#fff':'#000')};
-    box-shadow: 3px 3px 4px ${props =>(props.dark ? '#222' : '#ccc')};
-    padding: 4vh 5vw;
-    transition: linear 0.4s;
+export const Page = styled.div<CustomProps>`
+	width: 100%;
+	height: 100%;
+	background-color: ${(props) =>
+		props.dark ? "rgba(43, 43, 44,1)" : "#fafafa"};
+	overflow: hidden;
+`;
 
-    & > h3{
-        font-family: 'Syne', sans-serif;
-        font-weight: 700;
-        &:hover{cursor: pointer;}
-    }
-`
+export const Header = styled.div<CustomProps>`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	background-color: ${(props) => (props.dark ? "rgb(43, 43, 44)" : "#fff")};
+	color: ${(props) => (props.dark ? "#fff" : "#000")};
+	box-shadow: 3px 3px 4px ${(props) => (props.dark ? "#222" : "#ccc")};
+	padding: 4vh 5vw;
+	transition: linear 0.4s;
+
+	& > h3 {
+		font-family: "Syne", sans-serif;
+		font-weight: 700;
+		&:hover {
+			cursor: pointer;
+		}
+	}
+`;
 
 export const NavBar = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    justify-content: center;
-    text-align: center;
-    transition: linear 0.4s;
+	display: flex;
+	flex-direction: row;
+	align-items: flex-start;
+	justify-content: center;
+	text-align: center;
+	transition: linear 0.4s;
 
-    @media screen and (max-width: 601px) {
-            flex-direction: column;
-        } 
-`
+	@media screen and (max-width: 601px) {
+		flex-direction: column;
+	}
+`;
 
+export const Toggler = styled.div<CustomProps>`
+	color: ${(props) => (props.dark ? "#fff" : "#000")};
+	transition: linear 0.4s;
+	text-align: center;
+	${(props) => (props.show ? "transform: rotateZ(90deg);" : "")}
 
-export const Toggler = styled.div`
-    color: ${props =>(props.dark?'#fff':'#000')};
-    transition: linear 0.4s;
-    text-align: center;
-    ${props=>(props.show ? 'transform: rotateZ(90deg);':'')}
-     
-    &:hover{
-        cursor: pointer;
-        color: ${props =>(props.dark?'#aaa':'#444')};
-    }
+	&:hover {
+		cursor: pointer;
+		color: ${(props) => (props.dark ? "#aaa" : "#444")};
+	}
 
-    @media screen and (min-width: 601px) {
-        display: none;
-    }
-`
+	@media screen and (min-width: 601px) {
+		display: none;
+	}
+`;
 
-export const OptionsHolder = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    justify-content: center;
-    transition: linear 0.4s;
-    @media screen and (max-width: 601px) {
-            display: ${props=>(props.show ? 'flex':'none')};
-            position: absolute;
-            flex-direction: column;
-            top: 8vh;
-            background-color:${props =>(props.dark? '#2b3743':'white')}; 
-            transform: translateX(-43%);
-            border-radius: 1rem;;
-        }
-    
-`
+export const OptionsHolder = styled.div<CustomProps>`
+	display: flex;
+	flex-direction: row;
+	align-items: flex-start;
+	justify-content: center;
+	transition: linear 0.4s;
+	@media screen and (max-width: 601px) {
+		display: ${(props) => (props.show ? "flex" : "none")};
+		position: absolute;
+		flex-direction: column;
+		top: 8vh;
+		background-color: ${(props) => (props.dark ? "#2b3743" : "white")};
+		transform: translateX(-43%);
+		border-radius: 1rem;
+	}
+`;
 
-export const Options = styled.div`
-    padding: 0.5rem 1.5rem;
-    border-bottom: ${props => (props.activePage ? 'solid 2px red': 'none')};
-    color: ${props =>(props.dark?'#fff':'#000')};
-    background-image: linear-gradient(${props =>(props.dark?'rgba(255, 255, 255,0.1)':'rgba(0, 0, 0,0.1)')},${props =>(props.dark?'rgba(255, 255, 255,0.1)':'rgba(0, 0, 0,0.1)')});
-    background-size: 0 100%;
-    background-repeat: no-repeat;
-    transition: 0.4s;
-    &:hover{
-        cursor: pointer;
-        background-size: 100% 100%;
-        border-radius: 2rem;
-    }
-    @media screen and (max-width: 601px) {
-        width: 100%;
-        padding: 1rem 1rem;
-        &:hover{
-            border-radius: 1rem;
-        }
-    }
-`
+export const Options = styled.div<CustomProps & { activePage: boolean }>`
+	padding: 0.5rem 1.5rem;
+	border-bottom: ${(props) => (props.activePage ? "solid 2px red" : "none")};
+	color: ${(props) => (props.dark ? "#fff" : "#000")};
+	background-image: linear-gradient(
+		${(props) =>
+			props.dark ? "rgba(255, 255, 255,0.1)" : "rgba(0, 0, 0,0.1)"},
+		${(props) => (props.dark ? "rgba(255, 255, 255,0.1)" : "rgba(0, 0, 0,0.1)")}
+	);
+	background-size: 0 100%;
+	background-repeat: no-repeat;
+	transition: 0.4s;
+	&:hover {
+		cursor: pointer;
+		background-size: 100% 100%;
+		border-radius: 2rem;
+	}
+	@media screen and (max-width: 601px) {
+		width: 100%;
+		padding: 1rem 1rem;
+		&:hover {
+			border-radius: 1rem;
+		}
+	}
+`;
 
-
-
-export const DarkModeContainer= styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1vw;
-    cursor: pointer;
-    color: ${props =>(props.dark?'#fff':'rgba(0, 0, 0,0.4)')};
-    @media screen and (max-width: 750px) {
-      & > p{
-        display: none;
-      }
-    }
-`
+export const DarkModeContainer = styled.div<CustomProps>`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 1vw;
+	cursor: pointer;
+	color: ${(props) => (props.dark ? "#fff" : "rgba(0, 0, 0,0.4)")};
+	@media screen and (max-width: 750px) {
+		& > p {
+			display: none;
+		}
+	}
+`;
 
 export const MainContainer = styled.div`
-    width: 100%;
-    min-height: 90vh;
-    padding: 2vh 5vw;
-    background-image: url('./world.png');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-`
-
-
-
-
-
-
-
-
+	width: 100%;
+	min-height: 90vh;
+	padding: 2vh 5vw;
+	background-image: url("./world.png");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center;
+`;
