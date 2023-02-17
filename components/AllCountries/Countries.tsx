c7cd76bdc5c07c195e0aeaf223fdbe5c0a8b80c2
Masoud <masoud.zeinalabedini@gmail.com>
2023-02-17 15:47:30 +0330

    Fix unorderd country search

--- components/AllCountries/Countries.tsx
+++ components/AllCountries/Countries.tsx
@@ -1,88 +1,112 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {  Error, Grid, Loading } from './styles';
-import Country from '../cards/Country';
-import {RiLoader2Fill} from 'react-icons/ri'
-import { RegionContext } from '../helper/RegionContext';
-import { CountryContext } from '../helper/CountryContext';
-import { Dark } from '../helper/Dark';
+import React, { useState, useEffect, useContext } from "react";
+import { Error, Grid, Loading } from "./styles";
+import Country from "../cards/Country";
+import { RiLoader2Fill } from "react-icons/ri";
+import { RegionContext } from "../helper/RegionContext";
+import { CountryContext } from "../helper/CountryContext";
+import { Dark } from "../helper/Dark";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  const [showLoading, setshowLoading] = useState(true);
+  const [errorHandler, setErrorHandler] = useState("");
 
-    const [countries, setCountries] = useState([]);
-    const [showLoading, setshowLoading] = useState(true);
-    const [errorHandler, setErrorHandler] = useState('');
-    
-    const {region, setRegion}:any = useContext(RegionContext);
-    const {searchedCountry,setSearchedCountry}:any = useContext(CountryContext);
-    const {darkMode, setDarkMode}:any = useContext(Dark);
+  const { region, setRegion }: any = useContext(RegionContext);
+  const { searchedCountry, setSearchedCountry }: any =
+    useContext(CountryContext);
+  const { darkMode, setDarkMode }: any = useContext(Dark);
 
-    
-    useEffect(()=>{
-        if(region != ""){
-            setCountries([]);
-            fetch(`https://restcountries.com/v3.1/region/${region}`)
-            .then(res => res.json())
-            .then(data =>{data.status ? setErrorHandler('Country not Found')
-                : setCountries(data), setErrorHandler('');});
-                console.log("region");
-                
-        }
-        if(searchedCountry != ""){
-            setCountries([]);
-            setRegion("");
-            fetch(`https://restcountries.com/v3.1/name/${searchedCountry}`)
-            .then(res => res.json())
-            .then(data =>{ 
-                if(data.status) {
-                    setErrorHandler('Country not Found');setCountries([]);}
-                else {
-                    setCountries(data); setErrorHandler('');
-                }
-         });
-         console.log("country");
-        }
-        if(searchedCountry === "" && region === ""){
-            setCountries([]);
-            setErrorHandler('')
-            fetch(`https://restcountries.com/v3.1/all`)
-            .then(res => res.json())
-            .then(data =>{ setCountries(data); })
-            .catch(()=>setErrorHandler('Could not Connect to the Server'))
-            console.log("all"); 
-        } 
-    }, [region, searchedCountry])
-    
-    console.log(countries);
-    
-    const CountriesCard= countries.map((country:any) =>{ 
-        return(  
-           <Country
-             key={country.name.common} 
-             img={country.flags.png}
-             name={country.name.common}
-             population={country.population.toLocaleString()}
-             continent={country.continents[0]}
-             capital={country.capital}
-             cca={country.cca3} /> 
-             
-        )
+  let searchCount = 0;
+  let lastReturnedSearchCount = 0;
+  useEffect(() => {
+    if (region != "") {
+      setCountries([]);
+      fetch(`https://restcountries.com/v3.1/region/${region}`)
+        .then((res) => res.json())
+        .then((data) => {
+          data.status
+            ? setErrorHandler("Country not Found")
+            : setCountries(data),
+            setErrorHandler("");
+        });
+      console.log("region");
+    }
+    if (searchedCountry != "") {
+      setCountries([]);
+      setRegion("");
+      searchCount++;
+      fetchCountries(searchedCountry, searchCount)
+        .then((data) => {
+          if (data.searchCount > lastReturnedSearchCount) {
+            lastReturnedSearchCount = data.searchCount;
+            if (data.data.status) {
+              setErrorHandler("Country not Found");
+              setCountries([]);
+            } else {
+              setCountries(data.data);
+              setErrorHandler("");
+            }
+          }
+        })
+        .catch((err) => {
+          setErrorHandler("Could not Connect to The Server");
+          setCountries([]);
+        });
+    }
+    if (searchedCountry === "" && region === "") {
+      setCountries([]);
+      setErrorHandler("");
+      fetch(`https://restcountries.com/v3.1/all`)
+        .then((res) => res.json())
+        .then((data) => {
+          setCountries(data);
+        })
+        .catch(() => setErrorHandler("Could not Connect to the Server"));
+      console.log("all");
+    }
+  }, [region, searchedCountry]);
+
+  const fetchCountries = (
+    searchText: string,
+    searchCount: number
+  ): Promise<{ searchCount: number; data: any }> => {
+    return new Promise((resolve, reject) => {
+      fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+        .then((res) => res.json())
+        .then((data) => {
+          resolve({ searchCount, data });
+        })
+        .catch(() => {
+          reject();
+        });
     });
+  };
 
-    
-
+  const CountriesCard = countries.map((country: any) => {
     return (
-        <>
-            {countries.length == 0 && !errorHandler && <Loading dark={darkMode}>
-                <RiLoader2Fill />
-            </Loading>}
-            {errorHandler && <Error dark={darkMode}>
-                    {errorHandler}
-                </Error>}
-            <Grid>
-               {CountriesCard}
-            </Grid>
-        </>
+      <Country
+        key={country.name.common}
+        img={country.flags.png}
+        name={country.name.common}
+        population={country.population.toLocaleString()}
+        continent={country.continents[0]}
+        capital={country.capital}
+        cca={country.cca3}
+      />
     );
+  });
+
+  return (
+    <>
+      {countries.length == 0 && !errorHandler && (
+        <Loading dark={darkMode}>
+          <RiLoader2Fill />
+        </Loading>
+      )}
+      {errorHandler && <Error dark={darkMode}>{errorHandler}</Error>}
+      <Grid>{CountriesCard}</Grid>
+    </>
+  );
 };
 
 export default Countries;
--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,85 +1,110 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { BiSearchAlt2 } from 'react-icons/bi';
-import { RiLoader2Fill } from 'react-icons/ri';
-import Country from '../cards/Country';
-import { ActivePage } from '../helper/ActivePage';
-import { Dark } from '../helper/Dark';
-import { Loading, SearchBar, SearchBarContainer, SearchContainer, Error, Grid } from './style';
+import React, { useContext, useEffect, useState } from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { RiLoader2Fill } from "react-icons/ri";
+import Country from "../cards/Country";
+import { ActivePage } from "../helper/ActivePage";
+import { Dark } from "../helper/Dark";
+import {
+  Loading,
+  SearchBar,
+  SearchBarContainer,
+  SearchContainer,
+  Error,
+  Grid,
+} from "./style";
 
 const Search = () => {
+  const [searchedText, setsearchedText] = useState("");
+  const [countries, setCountries]: any = useState([]);
+  const [errorHandler, setErrorHandler]: any = useState("");
 
-    const [searchedText, setsearchedText] = useState('');
-    const [countries, setCountries]:any = useState([]);
-    const [errorHandler, setErrorHandler]:any = useState('');
+  const { darkMode, setDarkMode }: any = useContext(Dark);
+  const { active, setActive }: any = useContext(ActivePage);
 
-    const {darkMode, setDarkMode}:any = useContext(Dark);
-    const {active, setActive}:any = useContext(ActivePage);
-    
-    setActive('Search')
-
-    useEffect(()=>{
-        
-        fetch(`https://restcountries.com/v3.1/name/${searchedText}`)
-        .then(res => res.json())
-        .then(data =>{ 
-            if(data.status) {
-                setErrorHandler('Country not Found');setCountries([]);}
-            else {
-                setCountries(data); setErrorHandler('');
-            }
-        })
-        .catch(()=>{
-            setErrorHandler('Could not Connect to The Server');
+  setActive("Search");
+  let searchCount = 0;
+  let lastReturnedSearchCount = 0;
+  useEffect(() => {
+    searchCount++;
+    fetchCountries(searchedText, searchCount)
+      .then((data) => {
+        if (data.searchCount > lastReturnedSearchCount) {
+          lastReturnedSearchCount = data.searchCount;
+          if (data.data.status) {
+            setErrorHandler("Country not Found");
             setCountries([]);
-        });  
-    },[searchedText])
+          } else {
+            setCountries(data.data);
+            setErrorHandler("");
+          }
+        }
+      })
+      .catch((err) => {
+        setErrorHandler("Could not Connect to The Server");
+        setCountries([]);
+      });
+  }, [searchedText]);
 
-    let CountriesCard:any =[];
-
-    if(countries.length > 0 && searchedText.length > 0){
-        CountriesCard= countries.map((country:any) =>{ 
-        return(  
-           <Country
-             key={country.name.common} 
-             img={country.flags.png}
-             name={country.name.common}
-             population={country.population.toLocaleString()}
-             continent={country.continents[0]}
-             capital={country.capital}
-             cca={country.cca3} /> 
-             
-        )
+  const fetchCountries = (
+    searchText: string,
+    searchCount: number
+  ): Promise<{ searchCount: number; data: any }> => {
+    return new Promise((resolve, reject) => {
+      fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+        .then((res) => res.json())
+        .then((data) => {
+          resolve({ searchCount, data });
+        })
+        .catch(() => {
+          reject();
+        });
     });
-}
+  };
 
-    function onChangeCountry(event:any){
-        setsearchedText(event.target.value); 
-    }
-    
-    return (
-        <SearchContainer>
-            <SearchBarContainer dark={darkMode}>
-                <BiSearchAlt2 />
-                <SearchBar
-                 dark={darkMode}
-                 onChange={onChangeCountry}
-                 placeholder= 'Search for a country...'
-                 value={searchedText} />
-            </SearchBarContainer>
+  let CountriesCard: any = [];
 
-            {countries.length == 0 && !errorHandler && searchedText.length > 0 &&
-             <Loading dark={darkMode}>
-                <RiLoader2Fill />
-            </Loading>}
-            
-            {errorHandler && <Error dark={darkMode}>
-                   {errorHandler}
-                </Error>}
-            <Grid>
-               {CountriesCard}
-            </Grid>
-        </SearchContainer>
-    );
+  if (countries.length > 0 && searchedText.length > 0) {
+    CountriesCard = countries.map((country: any) => {
+      return (
+        <Country
+          key={country.name.common}
+          img={country.flags.png}
+          name={country.name.common}
+          population={country.population.toLocaleString()}
+          continent={country.continents[0]}
+          capital={country.capital}
+          cca={country.cca3}
+        />
+      );
+    });
+  }
+
+  function onChangeCountry(event: any) {
+    setsearchedText(event.target.value);
+  }
+
+  return (
+    <SearchContainer>
+      <SearchBarContainer dark={darkMode}>
+        <BiSearchAlt2 />
+        <SearchBar
+          dark={darkMode}
+          onChange={onChangeCountry}
+          placeholder="Search for a country..."
+          value={searchedText}
+        />
+      </SearchBarContainer>
+
+      {countries.length == 0 && !errorHandler && searchedText.length > 0 && (
+        <Loading dark={darkMode}>
+          <RiLoader2Fill />
+        </Loading>
+      )}
+
+      {errorHandler && <Error dark={darkMode}>{errorHandler}</Error>}
+      <Grid>{CountriesCard}</Grid>
+    </SearchContainer>
+  );
 };
 
 export default Search;
