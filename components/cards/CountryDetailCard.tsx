07b4019a52c1349f478ecac08c5870c0bba4a95d
Masoud <masoud.zeinalabedini@gmail.com>
2023-05-24 15:01:52 +0330

    Remove extral lines

--- components/cards/CountryDetailCard.tsx
+++ components/cards/CountryDetailCard.tsx
@@ -1,117 +1,113 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import { useContext, useEffect, useState } from 'react';
-import { Dark } from '../helper/Dark';
-import { Button, Flex, FlexColumn, Grid} from './styles';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
+import { css } from "@emotion/react";
+import { useContext, useEffect, useState } from "react";
+import { Dark } from "../helper/Dark";
+import { Button, Flex, FlexColumn, Grid } from "./styles";
+import Link from "next/link";
+import Image from "next/image";
 
-const CountryDetailCard = (props:any) => {
+const CountryDetailCard = (props: any) => {
+  const { darkMode, setDarkMode }: any = useContext(Dark);
+  const nativeName: any = Object.values(props.native)[0];
+  const currencies = Object.values(props.currencies).map(
+    (currency: any) => currency.name
+  );
+  const languages = Object.values(props.languages);
+  const [borderCountries, setBorderCountries] = useState<any[]>([]);
 
-    const {darkMode, setDarkMode}:any = useContext(Dark);
-    const nativeName:any = Object.values(props.native)[0];
-    const currencies = Object.values(props.currencies).map((currency:any)=>currency.name);
-    const languages = Object.values(props.languages);
-    const [borderCountries,setBorderCountries] = useState<any[]>([]);
-
-    const router = useRouter();
-
-
-
-
-    useEffect(()=>{
-       if(props.borders){
-            const totalCountofAllCountries = props.borders.length;
-            let allCountries:any[] = []
-            props.borders.map(async (countryCode:any)=>{
-                const resp = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
-                const countryJson = await resp.json()
-                allCountries.push(countryJson[0])
-                if(allCountries.length === totalCountofAllCountries){
-                    setBorderCountries(allCountries)
-                }
-            })
+  useEffect(() => {
+    if (props.borders) {
+      const totalCountofAllCountries = props.borders.length;
+      let allCountries: any[] = [];
+      props.borders.map(async (countryCode: any) => {
+        const resp = await fetch(
+          `https://restcountries.com/v3.1/alpha/${countryCode}`
+        );
+        const countryJson = await resp.json();
+        allCountries.push(countryJson[0]);
+        if (allCountries.length === totalCountofAllCountries) {
+          setBorderCountries(allCountries);
         }
-    },[props])
+      });
+    }
+  }, [props]);
 
-
-
-      const  borderCountriesBotton = borderCountries.map(borderCountry=>{
-            return(
-                <Link 
-                 key={borderCountry.cca3}
-                 href={`./${borderCountry.cca3}`}>
-                    <Button
-                     key={borderCountry.name.common} 
-                     dark={darkMode}>
-                        {borderCountry.name.common}
-                    </Button>
-                </Link>
-            )
-        });
-        
-
-    
+  const borderCountriesBotton = borderCountries.map((borderCountry) => {
     return (
-
-        <Grid>
-          <Image src={props.img} alt={props.name} width="100%" height="100%" /> 
-
-            <FlexColumn dark={darkMode}>
-                <h1>{props.name}</h1>
-                <Flex dark={darkMode}>
-                    <FlexColumn dark={darkMode}>
-                        <p>
-                            <b>Native Name: </b>
-                            {nativeName.common}
-                        </p>
-
-                        <p>
-                            <b>Population: </b>
-                            {props.population}
-                        </p>
-
-                        <p>
-                            <b>Region: </b>
-                            {props.region}
-                        </p>
-
-                        <p>
-                            <b>Sub Region: </b>
-                            {props.subregion}
-                        </p>
-
-                        <p>
-                            <b>Capital: </b>
-                            {props.capital}
-                        </p>
-                    </FlexColumn>
-
-                    <FlexColumn dark={darkMode}>
-                        <p>
-                            <b>Top Level Domain: </b>
-                            {props.tld}
-                        </p>
-
-                        <p>
-                            <b>Currencies: </b>
-                            {currencies}
-                        </p>
-
-                        <p>
-                            <b>Languages: </b>
-                            {languages.toString()}
-                        </p>
-                    </FlexColumn>
-                </Flex>
-
-                <Flex dark={darkMode} css={css`margin-top:0px;`}>
-                    <p><b>Border Countries: </b>{borderCountriesBotton}</p>
-                </Flex>
-            </FlexColumn>
-        </Grid>
+      <Link key={borderCountry.cca3} href={`./${borderCountry.cca3}`}>
+        <Button key={borderCountry.name.common} dark={darkMode}>
+          {borderCountry.name.common}
+        </Button>
+      </Link>
     );
+  });
+
+  return (
+    <Grid>
+      <Image src={props.img} alt={props.name} width="100%" height="100%" />
+
+      <FlexColumn dark={darkMode}>
+        <h1>{props.name}</h1>
+        <Flex dark={darkMode}>
+          <FlexColumn dark={darkMode}>
+            <p>
+              <b>Native Name: </b>
+              {nativeName.common}
+            </p>
+
+            <p>
+              <b>Population: </b>
+              {props.population}
+            </p>
+
+            <p>
+              <b>Region: </b>
+              {props.region}
+            </p>
+
+            <p>
+              <b>Sub Region: </b>
+              {props.subregion}
+            </p>
+
+            <p>
+              <b>Capital: </b>
+              {props.capital}
+            </p>
+          </FlexColumn>
+
+          <FlexColumn dark={darkMode}>
+            <p>
+              <b>Top Level Domain: </b>
+              {props.tld}
+            </p>
+
+            <p>
+              <b>Currencies: </b>
+              {currencies}
+            </p>
+
+            <p>
+              <b>Languages: </b>
+              {languages.toString()}
+            </p>
+          </FlexColumn>
+        </Flex>
+
+        <Flex
+          dark={darkMode}
+          css={css`
+            margin-top: 0px;
+          `}
+        >
+          <p>
+            <b>Border Countries: </b>
+            {borderCountriesBotton}
+          </p>
+        </Flex>
+      </FlexColumn>
+    </Grid>
+  );
 };
 
 export default CountryDetailCard;
